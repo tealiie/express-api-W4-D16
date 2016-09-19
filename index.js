471270c69789c68386110fd09718bf5e8b848b0b
EDA <eda@mac-mini-6.local>
2016-09-19 14:59:00 +1200

    getUser and deleteUser function complete

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var bodyParser = require('body-parser')
 var express = require('express')
-var hbs = require('express-handlebars')
 var path = require('path')
 
 var index = require('./routes/index')
@@ -8,12 +7,13 @@ var index = require('./routes/index')
 var PORT = 3000
 
 var app = express()
-app.engine('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', index.get)
+app.get('/:id', index.getUser)
+app.delete('/:id', index.deleteUser)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,47 @@ var development = require('../knexfile').development
 var knex = require('knex')(development)
 
 module.exports = {
-  get: get
+  get: get,
+  getUser: getUser,
+  deleteUser: deleteUser
 }
 
 function get (req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      res.json({ users: users })
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
+}
+
+function getUser (req,res) {
+  knex('users')
+  .select ()
+  .where ('id', '=', req.params.id)
+  .then (function (users) {
+    res.json (users[0])
+})
+  .catch(function (err) {
+    res.status(404).send('DATABASE ERROR: ' + err.messge)
+  })
+}
+
+function deleteUser (req, res) {
+  knex('users')
+  .select ()
+  .where ('id', '=', req.params.id)
+  .del ()
+  .then (function (delCount) {
+    if (delCount == 1) {
+      res.send (204)
+    } else {
+      res.send (404)
+    }
+  })
+  .catch(function (err) {
+    res.status(404).send('DATABASE ERROR: ' + err.message)
+  })
 }
